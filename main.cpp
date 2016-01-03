420ddeb69c5ccd1158c22c3c0ae8dddc33851847
Mr. J <jpjuecks@bju.edu>
2016-01-02 19:19:08 -0500

    Added some sample palette/image data handling...

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <array>
 #include <memory>
 #include <stdexcept>
 
@@ -67,22 +68,31 @@ static const struct sample_loc_t {
 
 static constexpr size_t NUM_SOUNDS = sizeof(samples) / sizeof(samples[0]);
 
-// Template specialization: have ALLEGRO_SAMPLE ptrs deleted with "al_destroy_sample(...)"
-namespace std {
-	template<>
-	class default_delete<ALLEGRO_SAMPLE>
-	{
-	public:
-		void operator()(ALLEGRO_SAMPLE *sptr) {
-			al_destroy_sample(sptr);
-		}
-	};
-}
+static constexpr size_t DEFAULT_PAL_OFFSET{ 636 };
+
+// Template specialization: have ALLEGRO_XXX ptr objectes "deleted" with al_destroy_xxx
+#define ALLEGRO_PTR_DELETER(struct_t, deleter_proc) \
+	namespace std { \
+		template<> \
+		class default_delete<struct_t> \
+		{ \
+		public: \
+			void operator()(struct_t *ptr) { \
+				deleter_proc(ptr); \
+			} \
+		}; \
+	}
+
+ALLEGRO_PTR_DELETER(ALLEGRO_DISPLAY, al_destroy_display)
+ALLEGRO_PTR_DELETER(ALLEGRO_BITMAP, al_destroy_bitmap)
+ALLEGRO_PTR_DELETER(ALLEGRO_SAMPLE, al_destroy_sample)
 
 // Use a vector of char for all file I/O (because stupid iostreams API)
 using Buffer = std::vector<char>;
 
 // Use unique_ptr to manage the lifetime of ALLEGRO_SAMPLE objects
+using DisplayPtr = std::unique_ptr<ALLEGRO_DISPLAY>;
+using BitmapPtr = std::unique_ptr<ALLEGRO_BITMAP>;
 using SamplePtr = std::unique_ptr<ALLEGRO_SAMPLE>;
 
 // Utility function to open and read the entire [binary] contents
@@ -104,6 +114,9 @@ class ResourceBin {
 
 	// Audio samples
 	std::vector<SamplePtr> wavs_;
+
+	// Default palette
+	std::array<ALLEGRO_COLOR, 256> default_pal_;
 public:
 	explicit ResourceBin(const char *pathToResourceBin = "RESOURCE.BIN") {
 		if (!slurp_file(pathToResourceBin, data_)) {
@@ -115,6 +128,19 @@ public:
 			void *sample_start = &data_[samples[i].offset];
 			wavs_.emplace_back(al_create_sample(sample_start, samples[i].length, FREQUENCY, ALLEGRO_AUDIO_DEPTH_UINT8, ALLEGRO_CHANNEL_CONF_1, false));
 		}
+
+		// Create default palette colors
+		for (size_t i = 0; i < 256; ++i) {
+			const char *cp = &data_[DEFAULT_PAL_OFFSET + (i * 3)];
+			unsigned char r = static_cast<unsigned char>(cp[0]) * 4;	// Original VGA palette entries are 0-63; we need 0-255
+			unsigned char g = static_cast<unsigned char>(cp[1]) * 4;
+			unsigned char b = static_cast<unsigned char>(cp[2]) * 4;
+			default_pal_[i] = al_map_rgb(r, g, b);
+		}
+	}
+
+	const ALLEGRO_COLOR& default_color(size_t index) const {
+		return default_pal_.at(index);
 	}
 
 	ALLEGRO_SAMPLE *sample(size_t index) const {
@@ -130,7 +156,25 @@ public:
 int main(int argc, char **argv) {
 	startup();
 
+	DisplayPtr dptr{ al_create_display(320, 200) };
+
 	ResourceBin rsrc{ "RESOURCE.BIN" };
+
+	Buffer sprites;
+	if (!slurp_file("SPRITES.BIN", sprites)) {
+		std::cout << "Failed to read SPRITES.BIN\n";
+		exit(1);
+	}
+
+	for (int y = 0; y < 200; ++y) {
+		for (int x = 0; x < 320; ++x) {
+			auto offset = (y * 320) + x + 7;	// 7 is BLOAD magic header size;
+			if (offset >= sprites.size()) { break; }
+			unsigned char color = static_cast<unsigned char>(sprites[offset]);
+			al_put_pixel(x, y, rsrc.default_color(color));
+		}
+	}
+	al_flip_display();
 
 	if (!al_reserve_samples(rsrc.num_samples())) {
 		std::cout << "Failed to reserve " << rsrc.num_samples() << " samples (errno=" << al_get_errno() << ")\n";
@@ -140,7 +184,7 @@ int main(int argc, char **argv) {
 	std::cout << "Enter a sample index [0, " << rsrc.num_samples() << "); invalid to quit: ";
 	int sound_index = 0;
 	while (std::cin >> sound_index) {
-		if ((sound_index < 0) || (sound_index >= rsrc.num_samples())) { break; }
+		if ((sound_index < 0) || (sound_index >= (int)rsrc.num_samples())) { break; }
 		al_play_sample(rsrc.sample(sound_index), 1.0f, ALLEGRO_AUDIO_PAN_NONE, 1.0f, ALLEGRO_PLAYMODE_ONCE, nullptr);
 		std::cout << "Enter a sample index [0, " << rsrc.num_samples() << "): ";
 	}
