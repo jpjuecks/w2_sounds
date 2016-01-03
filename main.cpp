9f790079aaedf7a119457c6499f7792a5d627cdd
Mr. J <jpjuecks@bju.edu>
2016-01-02 21:55:03 -0500

    Polished up sprite management and added multi palette support.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <memory>
 #include <stdexcept>
+#include <algorithm>
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_audio.h>
@@ -115,8 +116,8 @@ class ResourceBin {
 	// Audio samples
 	std::vector<SamplePtr> wavs_;
 
-	// Default palette
-	std::array<ALLEGRO_COLOR, 256> default_pal_;
+	// Palettes (256 colors each, although palettes 1 and 2 differ only in the 80 colors in the range 64..143)
+	std::array<std::array<ALLEGRO_COLOR, 256>, 3> palettes_;
 public:
 	explicit ResourceBin(const char *pathToResourceBin = "RESOURCE.BIN") {
 		if (!slurp_file(pathToResourceBin, data_)) {
@@ -130,17 +131,33 @@ public:
 		}
 
 		// Create default palette colors
+		size_t offset = DEFAULT_PAL_OFFSET;
 		for (size_t i = 0; i < 256; ++i) {
-			const char *cp = &data_[DEFAULT_PAL_OFFSET + (i * 3)];
+			const char *cp = &data_[offset];
 			unsigned char r = static_cast<unsigned char>(cp[0]) * 4;	// Original VGA palette entries are 0-63; we need 0-255
 			unsigned char g = static_cast<unsigned char>(cp[1]) * 4;
 			unsigned char b = static_cast<unsigned char>(cp[2]) * 4;
-			default_pal_[i] = al_map_rgb(r, g, b);
+			offset += 3;
+			palettes_[0][i] = al_map_rgba(r, g, b, (i == 0) ? 0 : 255);	// Color 0 is transparent!
+		}
+
+		// Create enemy palettes
+		for (size_t p = 1; p < 3; ++p) {
+			std::copy(palettes_[0].begin(), palettes_[0].begin() + 64, palettes_[p].begin());			// 0..63 copied
+			for (size_t i = 64; i < 144; ++i) {
+				const char *cp = &data_[offset];
+				unsigned char r = static_cast<unsigned char>(cp[0]) * 4;
+				unsigned char g = static_cast<unsigned char>(cp[1]) * 4;
+				unsigned char b = static_cast<unsigned char>(cp[2]) * 4;
+				offset += 3;
+				palettes_[p][i] = al_map_rgba(r, g, b, 255);
+			}
+			std::copy(palettes_[0].begin() + 144, palettes_[0].end(), palettes_[p].begin() + 144);		// 144..255 copied
 		}
 	}
 
-	const ALLEGRO_COLOR& default_color(size_t index) const {
-		return default_pal_.at(index);
+	const ALLEGRO_COLOR& color(size_t index, size_t palette=0) const {
+		return palettes_.at(palette).at(index);
 	}
 
 	ALLEGRO_SAMPLE *sample(size_t index) const {
@@ -150,43 +167,83 @@ public:
 	size_t num_samples() const {
 		return NUM_SOUNDS;
 	}
+};
 
+class SpritesBin {
+	// Bitmaps containing all the data from SPRITES.BIN in the 3 available palettes
+	std::array<BitmapPtr, 3> sprite_maps_;
+public:
+	// Must have loaded palette data from RESOURCE.BIN first!
+	SpritesBin(const ResourceBin& rsrc, const char *pathToSpritesBin = "SPRITES.BIN") {
+		Buffer raw;
+		if (!slurp_file(pathToSpritesBin, raw)) {
+			throw std::exception("Unable to read data from SPRITES.BIN");
+		}
+
+		for (size_t i = 0; i < 3; ++i) {
+			sprite_maps_[i].reset(al_create_bitmap(320, 200));
+			if (!sprite_maps_[i].get()) {
+				throw std::exception("Unable to create bitmap");
+			}
+			if (!al_lock_bitmap(sprite_maps_[i].get(), ALLEGRO_PIXEL_FORMAT_ANY, ALLEGRO_LOCK_WRITEONLY)) {
+				throw std::exception("Unable to lock bitmap");
+			}
+			al_set_target_bitmap(sprite_maps_[i].get());
+			al_clear_to_color(rsrc.color(0, i));
+			for (int y = 0; y < 200; ++y) {
+				for (int x = 0; x < 320; ++x) {
+					size_t offset = (y * 320) + x + 7;	// 7 is BLOAD magic header size;
+					if (offset >= raw.size()) { break; }
+					unsigned char color = static_cast<unsigned char>(raw[offset]);
+					al_put_pixel(x, y, rsrc.color(color, i));
+				}
+			}
+			al_unlock_bitmap(sprite_maps_[i].get());
+		}
+	}
+
+	ALLEGRO_BITMAP *sprite_map(size_t palette = 0) {
+		return sprite_maps_.at(palette).get();
+	}
 };
 
 int main(int argc, char **argv) {
 	startup();
 
-	DisplayPtr dptr{ al_create_display(320, 200) };
+	DisplayPtr dptr{ al_create_display(640, 400) };
 
+	auto t1 = al_get_time();
 	ResourceBin rsrc{ "RESOURCE.BIN" };
+	auto t2 = al_get_time();
+	SpritesBin sprites{ rsrc, "SPRITES.BIN" };
+	auto t3 = al_get_time();
+	std::cout << "t2 - t1: " << t2 - t1 << std::endl;
+	std::cout << "t3 - t2: " << t3 - t2 << std::endl;
+	std::cout << "t3 - t1: " << t3 - t1 << std::endl;
 
-	Buffer sprites;
-	if (!slurp_file("SPRITES.BIN", sprites)) {
-		std::cout << "Failed to read SPRITES.BIN\n";
-		exit(1);
-	}
-
-	for (int y = 0; y < 200; ++y) {
-		for (int x = 0; x < 320; ++x) {
-			auto offset = (y * 320) + x + 7;	// 7 is BLOAD magic header size;
-			if (offset >= sprites.size()) { break; }
-			unsigned char color = static_cast<unsigned char>(sprites[offset]);
-			al_put_pixel(x, y, rsrc.default_color(color));
-		}
-	}
-	al_flip_display();
-
+	
 	if (!al_reserve_samples(rsrc.num_samples())) {
 		std::cout << "Failed to reserve " << rsrc.num_samples() << " samples (errno=" << al_get_errno() << ")\n";
 		exit(1);
 	}
 
+	al_set_target_backbuffer(dptr.get());
+
 	std::cout << "Enter a sample index [0, " << rsrc.num_samples() << "); invalid to quit: ";
 	int sound_index = 0;
+	size_t pal = 0;
+	al_clear_to_color(al_map_rgb(128, 128, 128));
+	al_draw_scaled_bitmap(sprites.sprite_map(pal), 0, 0, 16, 16, 50, 50, 100, 100, 0);
+	al_flip_display();
 	while (std::cin >> sound_index) {
 		if ((sound_index < 0) || (sound_index >= (int)rsrc.num_samples())) { break; }
 		al_play_sample(rsrc.sample(sound_index), 1.0f, ALLEGRO_AUDIO_PAN_NONE, 1.0f, ALLEGRO_PLAYMODE_ONCE, nullptr);
 		std::cout << "Enter a sample index [0, " << rsrc.num_samples() << "): ";
+
+		pal = (pal < 2) ? (pal + 1) : 0;
+		al_clear_to_color(al_map_rgb(128, 128, 128));
+		al_draw_scaled_bitmap(sprites.sprite_map(pal), 0, 0, 320, 200, 0, 0, 640, 400, 0);
+		al_flip_display();
 	}
 
 	return 0;
